7a775c55c41c17663a680584225a6bf3fda8fbe5
yeon-dong <rchijy@naver.com>
2024-06-03 16:01:28 +0900

    FEAT Main

--- For_The_P/src/App.jsx
+++ For_The_P/src/App.jsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import CalendarPage from "./pages/CalendarPage/CalendarPage";
+import TodoPage from "./pages/TodoPage/TodoPage";
+import SettingPage from "./pages/SettingPage/SettingPage";
+import SettingFriendPage from "./pages/SettingFriendPage/SettingFriendPage";
+import SettingCategoryPage from "./pages/SettingCategoryPage/SettingCategoryPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import './App.css'
 
 function App() {
   return (
-    <><AuthProvider>
+    <>
       <div className='root-wrap'>
       <BrowserRouter>
         <Header />
@@ -11,16 +19,15 @@ function App() {
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/calender" element={<CalenderPage />}></Route>
-          <Route path="/todo" element={<NowPlayingPage />}></Route>
-          <Route path="/setting" element={<PopularPage />}></Route>
-          <Route path="/setting/friend" element={<TopRatedPage />}></Route>
-          <Route path="/setting/category" element={<UpComingPage />}></Route>
+          <Route path="/calender" element={<CalendarPage />}></Route>
+          <Route path="/todo" element={<TodoPage />}></Route>
+          <Route path="/setting" element={<SettingPage />}></Route>
+          <Route path="/setting/friend" element={<SettingFriendPage />}></Route>
+          <Route path="/setting/category" element={<SettingCategoryPage />}></Route>
           <Route path="/*" element={<NotFoundPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
-    </AuthProvider>
     </>
   )
 }

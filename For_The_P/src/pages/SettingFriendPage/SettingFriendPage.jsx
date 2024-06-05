4f8089e2ba249e7f2aa21089cfc1314de9150052
yeon-dong <rchijy@naver.com>
2024-06-05 17:51:33 +0900

    FEAT Setting Friend Page

--- For_The_P/src/pages/SettingFriendPage/SettingFriendPage.jsx
+++ For_The_P/src/pages/SettingFriendPage/SettingFriendPage.jsx
@@ -1,12 +1,42 @@
 import Header from "../../components/Header/Header";
 import { Link } from "react-router-dom";
-import { SettingContainer, SettingText, BackArrow, SearchBox } from "./SettingFriendPage.style";
-import { Input } from "antd";
+import { SettingContainer, SettingText, BackArrow, SearchBox, FriendIMG, FriendNameText, FriendContainer, FriendBox, FriendDelButton, FriendInfo } from "./SettingFriendPage.style";
+import { Input, Modal, Button } from "antd";
+import { useState, useCallback } from "react";
 const { Search } = Input;
 const onSearch = (value, _e, info) => console.log(info?.source, value);
 
-function SettingFriendPage() {
 
+
+function SettingFriendPage() {
+  const [friends, setFriend] = useState([
+    { id: 1, name: "윤예준" },
+    { id: 2, name: "고재원" },
+    { id: 3, name: "일정 마스터" },
+    { id: 4, name: "졸업 작품 마스터" },
+  ]);
+  const [friendToDel,setFriendToDel] = useState(0);
+
+  const delFriend = useCallback(
+    (id) => {
+      setFriend(friends.filter((friend) => friend.id !== id));
+    },
+    [friends],
+  );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = (id) => {
+    setFriendToDel(id);
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    delFriend(friendToDel);
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
     return (
       <>
@@ -23,6 +53,15 @@ function SettingFriendPage() {
             onSearch={onSearch}
           />
           </SearchBox>
+          {friends.map((friend) => (<FriendBox key={friend.id}><FriendContainer>
+          <FriendInfo><FriendIMG src="/user_img.svg"/>
+          <FriendNameText>{friend.name}</FriendNameText></FriendInfo>
+          <FriendDelButton onClick={() => showModal(friend.id)}>친구 삭제</FriendDelButton>
+          </FriendContainer>
+          </FriendBox>))}
+          <Modal title="친구를 정말 삭제하시겠습니까?" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <p>{friends.find(friend => friend.id === friendToDel)?.name}님의 친구 목록에서도 삭제됩니다.</p>
+          </Modal>
         </SettingContainer>
       </>
     );
--- For_The_P/src/pages/SettingFriendPage/SettingFriendPage.style.js
+++ For_The_P/src/pages/SettingFriendPage/SettingFriendPage.style.js
@@ -17,11 +17,52 @@ export const SettingText = styled.div`
     font-family: "Gothic A1", sans-serif;
     font-weight: 700;
     font-style: normal;
-    font-size : 40px;
-    color: black;
+    font-size : 40px;    color: black;
 `;
 
 export const SearchBox = styled.div`
     width: 50%;
     margin-top: 24px;
+`;
+
+export const FriendBox = styled.div`
+    width: 50vw;
+`;
+
+export const FriendContainer = styled.div`
+    margin-top: 12px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const FriendIMG = styled.img`
+    height: 96px;
+    width: 96px;
+`;
+
+export const FriendNameText = styled.div`
+    font-family: "Gothic A1", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    font-size : 40px;
+    color: black;
+`;
+
+export const FriendInfo = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+
+export const FriendDelButton = styled.button`
+    font-family: "Gothic A1", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    font-size : 40px;
+    color: #A60A33;
+    border-color: #A60A33;
+    padding: 8px;
 `;
